58833ba00bff746e58770c590783f6d4e63bc5e6
farid45219 <refrigerator.rni.eed7@waltonplc.com>
2025-01-12 15:10:49 +0600

    LPF added

--- ADXL345_W25Q64FVSIG.ino
+++ ADXL345_W25Q64FVSIG.ino
@@ -4,9 +4,20 @@
 #include <util/delay.h>
 #include "i2c.h"
 #include "adxl345.h"
+#include "lpf.h"
+
 #define  F_CPU 8000000UL
 
+
+int acc_x = 0, acc_y = 0, acc_z = 0;
+
 void setup() {
+
+  LPF_Init();
+  LPF_Set_Alpha(0, 20);
+  LPF_Set_Alpha(1, 20);
+  LPF_Set_Alpha(2, 20);
+
   Serial.begin(38400);
   cli();
   CLKPR = (1<<CLKPCE);
@@ -18,11 +29,18 @@ void setup() {
 
 void loop() {
   ADXL345_Read_Acc();
-  Serial.print(ADXL345_Get_Acc_X());
+  acc_x = ADXL345_Get_Acc_X();
+  acc_y = ADXL345_Get_Acc_Y();
+  acc_z = ADXL345_Get_Acc_Z();
+  acc_x = LPF_Get_Filtered_Value(0, acc_x);
+  acc_y = LPF_Get_Filtered_Value(1, acc_y);
+  acc_z = LPF_Get_Filtered_Value(2, acc_z);
+
+  Serial.print(acc_x);
   Serial.print(",");
-  Serial.print(ADXL345_Get_Acc_Y());
+  Serial.print(acc_y);
   Serial.print(",");
-  Serial.print(ADXL345_Get_Acc_Z());
+  Serial.print(acc_z);
   Serial.println();
   delay(20);
 }
---
+++ lpf.h
@@ -0,0 +1,15 @@
+
+/* 
+ * File:   lpf.h
+ * Author: MD. Faridul Islam
+ * IIR Butterworth LPF Library
+ * Created on May 1, 2023, 19:00
+ */
+ 
+ 
+
+void    LPF_Struct_Init(void);
+void    LPF_Set_Alpha(uint8_t lpf_index, uint8_t val);
+uint8_t LPF_Get_Alpha(uint8_t lpf_index);
+int32_t LPF_Get_Filtered_Value(uint8_t lpf_index, int32_t val);
+void    LPF_Init(void);
